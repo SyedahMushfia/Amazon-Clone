fdb2fc2b2f06f1c0c00aba74d26e942ed4dc1784
Syedah Mushfia <syedahshifa@ymail.com>
2024-07-06 19:01:21 +0530

    Add routing in App component
    - Replace direct import of Home component with routing setup using createBrowserRouter
    - Integrate routing with RouterProvider to handle navigation
    - Set up routes for Home, CheckoutPage, ProductListing, and NotFoundPage
    - Implement lazy loading for SignIn component
    - Include Header, ScrollToTop, and FooterNav components in the LayoutComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,68 @@
+import Home from "./components/home/Home";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { ImageStylingContext, images } from "./context/ImageStylingContext";
+import CheckoutPage from "./components/checkoutPage/CheckoutPage";
+import NotFoundPage from "./components/NotFoundPage";
+import ProductListing from "./components/productsPage/productListingPage/ProductListing";
 import Header from "./components/header/Header";
-// import Home from "./components/home/Home";
-// import { ImageStylingContext, images } from "./context/ImageStylingContext";
+import { lazy, Suspense } from "react";
+const SignIn = lazy(() => import("./components/home/SignIn"));
+import CheckVisibilityWrapper from "./components/home/CheckVisibilityWrapper";
+import LoadingCircle from "./components/home/LoadingCircle";
+import ScrollToTop from "./components/footer/ScrollToTop";
+import FooterNav from "./components/footer/FooterNav";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <ImageStylingContext.Provider value={images}>
+        <LayoutComponent />
+      </ImageStylingContext.Provider>
+    ),
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/checkout",
+        element: <CheckoutPage />,
+      },
+      {
+        path: "/productListing/:id",
+        element: <ProductListing />,
+      },
+    ],
+  },
+  {
+    path: "/notFoundPage",
+    element: <NotFoundPage />,
+  },
+]);
+
+function LayoutComponent() {
   return (
-    <div className="app">
+    <div>
       <Header />
-      
-      {/* <ImageStylingContext.Provider value={images}>
-        <Home />
-      </ImageStylingContext.Provider> */}
+      <main>
+        <Outlet />
+      </main>
+      {/* This wrapper is used to check component visibility. */}
+      <CheckVisibilityWrapper>
+        <Suspense fallback={<LoadingCircle />}>
+          <SignIn />
+        </Suspense>
+      </CheckVisibilityWrapper>
+      <ScrollToTop />
+      <FooterNav />
     </div>
   );
+}
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
