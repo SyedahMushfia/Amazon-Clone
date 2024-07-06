3a9c6f849874840e7990013eaa201f24d29d3e18
Syedah Mushfia <syedahshifa@ymail.com>
2024-07-06 14:22:13 +0530

    Add LoadingCircle component
    - This component is used for displaying a loading indicator when lazyloading other components.

---
+++ src/components/home/LoadingCircle.tsx
@@ -0,0 +1,9 @@
+function LoadingCircle() {
+  return (
+    <div className="flex justify-center items-center h-[20vw]">
+      <div className="w-[6%] h-[30%] border-[12px] border-gray-400 rounded-full animate-spin"></div>
+    </div>
+  );
+}
+
+export default LoadingCircle;
