4e301d9fe1d376f6a5446b81281644001e7cdef1
Syedah Mushfia <syedahshifa@ymail.com>
2024-07-09 13:10:43 +0530

    Update StarRating component to include half-star ratings
    - Modify the logic to determine if a full star, half star, or empty star should be rendered based on the rating.

--- src/components/productsPages/productListingPage/sidebar/StarRating.tsx
+++ src/components/productsPages/productListingPage/sidebar/StarRating.tsx
@@ -1,5 +1,6 @@
 import StarIcon from "@mui/icons-material/Star";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarHalfIcon from "@mui/icons-material/StarHalf";
 
 interface StarRatingProps {
   rating: number;
@@ -9,30 +10,48 @@ interface StarRatingProps {
 function StarRating(props: StarRatingProps) {
   const rating = props.rating;
   const totalStars = 5;
+  const stars = [];
 
-  return (
-    <div className="flex">
-      {[...Array(totalStars)].map((star, index) => {
-        return (
-          <div className="-mt-[3%] -ml-[2%] text-amber-500 hover:cursor-pointer">
-            {index < rating ? (
-              <StarIcon
-                style={{
-                  fontSize: "clamp(0.5625rem, 0.1884rem + 1.5962vi, 1.625rem)",
-                }}
-              />
-            ) : (
-              <StarBorderIcon
-                style={{
-                  fontSize: "clamp(0.5625rem, 0.1884rem + 1.5962vi, 1.625rem)",
-                }}
-              />
-            )}
-          </div>
-        );
-      })}
-    </div>
-  );
+  // To generate star icons based on rating
+  for (let i = 0; i < totalStars; i++) {
+    let star: React.ReactElement;
+
+    if (rating - i > 0 && rating - i < 1) {
+      star = (
+        <StarHalfIcon
+          key={i}
+          style={{
+            fontSize: "clamp(0.5625rem, 0.1884rem + 1.5962vi, 1.625rem)",
+          }}
+          className="text-amber-500 hover:cursor-pointer"
+        />
+      );
+    } else if (i < rating) {
+      star = (
+        <StarIcon
+          key={i}
+          style={{
+            fontSize: "clamp(0.5625rem, 0.1884rem + 1.5962vi, 1.625rem)",
+          }}
+          className="text-amber-500 hover:cursor-pointer"
+        />
+      );
+    } else {
+      star = (
+        <StarBorderIcon
+          key={i}
+          style={{
+            fontSize: "clamp(0.5625rem, 0.1884rem + 1.5962vi, 1.625rem)",
+          }}
+          className="text-amber-500 hover:cursor-pointer"
+        />
+      );
+    }
+
+    stars.push(star);
+  }
+
+  return <div>{stars}</div>;
 }
 
 export default StarRating;
