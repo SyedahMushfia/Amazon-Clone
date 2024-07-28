1a771b22e31a0b3d8add7cbbcd19ca389670a581
Syedah Mushfia <syedahshifa@ymail.com>
2024-07-28 22:13:49 +0530

    Add StarRating component to display star icons based on rating value

---
+++ src/components/productsPages/StarRating.tsx
@@ -0,0 +1,53 @@
+import StarIcon from "@mui/icons-material/Star";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarHalfIcon from "@mui/icons-material/StarHalf";
+
+interface StarRatingProps {
+  rating: number; // The rating value to display
+  totalStars?: number; // Optional prop for the total number of stars
+  fontSize: string; // The size of the star icons
+}
+
+function StarRating(props: StarRatingProps) {
+  const rating = props.rating; // Extract rating from props
+  const totalStars = 5;
+  const stars = [];
+
+  // To generate star icons based on rating
+  for (let i = 0; i < totalStars; i++) {
+    let star: React.ReactElement;
+
+    // Conditionally render full, half, or empty star icons
+    if (rating - i > 0 && rating - i < 1) {
+      star = (
+        <StarHalfIcon
+          key={i}
+          style={{ fontSize: props.fontSize }}
+          className="text-amber-500 hover:cursor-pointer -mr-[3%]"
+        />
+      );
+    } else if (i < rating) {
+      star = (
+        <StarIcon
+          key={i}
+          style={{ fontSize: props.fontSize }}
+          className="text-amber-500 hover:cursor-pointer -mr-[3%]"
+        />
+      );
+    } else {
+      star = (
+        <StarBorderIcon
+          key={i}
+          style={{ fontSize: props.fontSize }}
+          className="text-amber-500 hover:cursor-pointer -mr-[3%]"
+        />
+      );
+    }
+
+    stars.push(star); // Add star to the stars array
+  }
+
+  return <div className="flex">{stars}</div>;
+}
+
+export default StarRating;
